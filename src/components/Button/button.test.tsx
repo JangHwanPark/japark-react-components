d740c12d6dd2fd3de9fd08c5ecff1ed986f25bcf
JangHwan <wkd5878@gmail.com>
2025-02-23 09:59:53 +0900

    :art: chore - viconfig 내 테스트 설정 추가

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -5,9 +5,9 @@ import Button from './index';
 // 스냅샷 : 컴포넌트의 출력(UI 구조)이 변경되었는지 확인
 describe('Button 컴포넌트 테스트', () => {
   it('버튼이 일치하는지 확인 (스냅샷)', () => {
-    // render(<Button text='text'/>);
-    // expect(screen.getByText('text')).toMatchSnapshot();
-    const { container } = render(<Button text="text" />);
-    expect(container.firstChild).toMatchSnapshot();
+    const { container } = render(<Button text='text'/>);
+    
+    // 스냅샷 테스트 : container의 둠 구조를 기록하여 비교
+    expect(container).toMatchSnapshot();
   })
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
@@ -8,4 +9,13 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+
+  // test.globals 옵션은 테스트 파일의 전역 함수를 import 없이 사용할 수 있게 합니다.
+  // test.environment 옵션은 테스트 환경을 지정합니다.
+  // test.setupFiles는 테스트 전에 실행될 파일을 지정합니다.
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['setupTests.tsx']
+  }
 })
