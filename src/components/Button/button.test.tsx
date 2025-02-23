bf22a436eb3f45485dc7c33824f3a70d303dfd7d
JangHwan <wkd5878@gmail.com>
2025-02-23 09:42:43 +0900

    :art: chore - 테스트 스위트 명칭 변경

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Button from './index';
 
 // 스냅샷 : 컴포넌트의 출력(UI 구조)이 변경되었는지 확인
-describe('<Button/>', () => {
+describe('Button 컴포넌트 테스트', () => {
   it('버튼이 일치하는지 확인 (스냅샷)', () => {
     render(<Button text='text'/>);
     expect(screen.getByText('text')).toMatchSnapshot();
