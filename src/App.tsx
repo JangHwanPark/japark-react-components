754c66b2828519de9d28e99450511d8d2891ac58
JangHwan <wkd5878@gmail.com>
2025-03-08 13:14:49 +0900

    :sparkle: feat - Toast 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import Toast from './Feedback/Toast/Toast';
 function App() {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-[#0f1214]">
-      <div className="flex gap-4 p-4">
-        <Toast message="default Type" />
-        <Toast message="success" type="success" />
+      <div className="w-full max-w-3xl mx-auto flex flex-col gap-4 p-4">
+        <Toast message="info" type="info" size="sm" />
+        <Toast message="success" type="success" size="sm" />
         <Toast message="error" type="error" />
-        <Toast message="warning" type="warning" />
+        <Toast message="warning" type="warning" size="lg" />
       </div>
     </div>
   );
--- src/Feedback/Toast/Toast.tsx
+++ src/Feedback/Toast/Toast.tsx
@@ -1,33 +1,51 @@
-import { forwardRef } from 'react';
+import { forwardRef, ReactNode } from 'react';
 import clsx from 'clsx';
+import { FaInfo, FaRegCheckCircle } from 'react-icons/fa';
+import { MdErrorOutline } from 'react-icons/md';
+import { LuMessageCircleWarning } from 'react-icons/lu';
 
 interface ToastProps {
   id?: string;
   message: string;
-  type?: 'success' | 'error' | 'warning';
+  icon?: ReactNode;
+  type?: 'info' | 'success' | 'error' | 'warning';
   size?: 'sm' | 'md' | 'lg';
 }
 
 const Toast = forwardRef<any, ToastProps>(
   (
-    { id = `toast-${Math.random().toString(36).substr(2, 9)}`, message, type = 'success', size },
+    {
+      id = `toast-${Math.random().toString(36).substr(2, 9)}`,
+      message,
+      icon,
+      type = 'success',
+      size = 'md',
+    },
     ref,
   ) => {
     const toastClass = clsx(
+      'w-full px-6 py-4 flex items-center gap-5 rounded-lg',
       {
-        'bg-green-500': type === 'success',
-        'bg-red-500': type === 'error',
-        'bg-yellow-500': type === 'warning',
+        'bg-primary': type === 'info',
+        'bg-success': type === 'success',
+        'bg-danger': type === 'error',
+        'bg-warning': type === 'warning',
       },
       {
-        'text-md': size === 'sm',
-        'text-lg': size === 'md',
-        'text-xl': size === 'lg',
+        'text-lg': size === 'sm',
+        'text-xl': size === 'md',
+        'text-2xl': size === 'lg',
       },
     );
+
     return (
       <div id={id} ref={ref} className={toastClass}>
-        <span>{message}</span>
+        {!icon && type === 'info' && <FaInfo />}
+        {!icon && type === 'success' && <FaRegCheckCircle />}
+        {!icon && type === 'error' && <MdErrorOutline />}
+        {!icon && type === 'warning' && <LuMessageCircleWarning />}
+        {icon && icon}
+        <p className="pb-1">{message}</p>
       </div>
     );
   },
