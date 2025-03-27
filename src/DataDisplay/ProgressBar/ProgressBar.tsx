2ed5f1f174dd831f8ddd29dc1b535fd989144374
japark <wkd5878@gmail.com>
2025-03-27 14:41:29 +0900

    :sparkle: feat - 와이어 프레임 추가
    - Button

--- src/DataDisplay/ProgressBar/ProgressBar.tsx
+++ src/DataDisplay/ProgressBar/ProgressBar.tsx
@@ -1,3 +1,10 @@
+import { RefObject } from "react";
+
+interface ProgressBarProps {
+ ref: RefObject<HTMLDivElement | null>;
+ className?: string;
+}
+
 const ProgressBar = ({ progress }: { progress: number }) => {
   return (
     <div className="w-full bg-gray-300 rounded-full h-4">
---
+++ src/WireFrame/Buttons/Button.tsx
@@ -0,0 +1,10 @@
+const Button = () => {
+ return (
+  <div>
+   <div></div>
+   <div></div>
+  </div>
+ );
+};
+
+export default Button;
